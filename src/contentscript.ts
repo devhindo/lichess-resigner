4e46f7723942174e6753082314ecc9b145b43301
hindawi <ahmedhindawi2001@gmail.com>
2023-06-02 00:44:08 +0300

    done for today

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,1 +1,44 @@
 // code that runs in the current opened tab
+/*
+var timers = document.getElementsByClassName("time") as HTMLCollectionOf<HTMLElement>;
+var timer = timers[1] as HTMLElement;
+const isTenth = timer.getElementsByTagName("tenths")
+if(isTenth.length > 0) {
+    var sec = isTenth[0]?.textContent;
+    const newSec = sec?.slice(1);
+    var secInt = parseInt(newSec ?? '0');
+    if(secInt == 3) {
+        // resign
+        var resignElements = document.getElementsByClassName("resign") as HTMLCollectionOf<HTMLElement>;
+        var resignButton = resignElements[0] as HTMLElement;
+        resignButton.click();
+        resignButton.click();
+    }
+}
+*/
+console.log("extension loaded - lichess resigner");
+
+
+
+
+
+
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if(mutation.type === 'childList') {
+            const elements = document.getElementsByClassName("time");
+            const timer = elements[1];
+            const isTenth = timer.getElementsByTagName("tenths");
+            if(isTenth.length > 0) {
+                console.log("checking if tenth is available");
+                var resignElements = document.getElementsByClassName("resign") as HTMLCollectionOf<HTMLElement>;
+                var resignButton = resignElements[0] as HTMLElement;
+                resignButton.click();
+                resignButton.click();
+            } else {
+                console.log("checking");
+            }
+        }
+    });
+});
+
