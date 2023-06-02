8e6a3f2cf967c39735b74f451e1934da801194b7
hindawi <ahmedhindawi2001@gmail.com>
2023-06-02 19:25:17 +0300

    added more functionality

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -25,16 +25,25 @@ console.log("extension loaded - lichess resigner");
 
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-        if(mutation.type === 'childList') {
+        if (mutation.type === 'childList') {
             const elements = document.getElementsByClassName("time");
             const timer = elements[1];
             const isTenth = timer.getElementsByTagName("tenths");
-            if(isTenth.length > 0) {
+            if (isTenth.length > 0) {
                 console.log("checking if tenth is available");
-                var resignElements = document.getElementsByClassName("resign") as HTMLCollectionOf<HTMLElement>;
-                var resignButton = resignElements[0] as HTMLElement;
-                resignButton.click();
-                resignButton.click();
+                const timerString = timer.textContent;
+                const [minutes, secondsWithMilliseconds] = timerString?.split(':') as  string[];
+                const [seconds, milliseconds] = secondsWithMilliseconds.split('.') as string[];
+
+                const minutesInt = parseInt(minutes, 10);
+                const secondsInt = parseInt(seconds, 10);
+                const millisecondsInt = parseInt(milliseconds, 10);
+                if(secondsInt < 2) {
+                    var resignElements = document.getElementsByClassName("resign") as HTMLCollectionOf<HTMLElement>;
+                    var resignButton = resignElements[0] as HTMLElement;
+                    resignButton.click();
+                    resignButton.click();
+                }
             } else {
                 console.log("checking");
             }
